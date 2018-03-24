8a0d1e1d0652091954dfe1018623f342aa0d3c8d
jensgholm15@gmail.com <jensgholm15@gmail.com>
2018-03-24 20:47:43 +0100

    Add text alignment option in text display

--- assets/Logic/Test/TextDisplay.cpp
+++ assets/Logic/Test/TextDisplay.cpp
@@ -16,13 +16,12 @@ void TextDisplay::Start()
 
     m_Material.GetUniformContext().SetTexture(std::hash<std::string>()("_TextureAtlas"), fontTexture->GetTexture());
 
-    fontTexture->GenerateTextMesh(m_Mesh, m_DisplayText);
+    fontTexture->GenerateTextMesh(m_Mesh, m_DisplayText, m_TextAlign);
+    glm::vec3 textMeshBounds = m_Mesh.GetBounds();
 
     MeshRenderComponent *meshRenderer = AddComponent<MeshRenderComponent>();
 
     meshRenderer->mesh = &m_Mesh;
     meshRenderer->material = &m_Material;
     meshRenderer->renderQueue = RenderQueue::Overlay;
-
-    DebugLog("Yaw!");
 }
--- assets/Logic/Test/TextDisplay.hpp
+++ assets/Logic/Test/TextDisplay.hpp
@@ -12,6 +12,7 @@ class TextDisplay : public LogicComponent
 public:
     TextDisplay()
     {
+        m_TextAlign = TextAlign::Left;
     }
 
     void Update() override;
@@ -21,9 +22,26 @@ public:
     void Deserialize(SerializerType serializer)
     {
         DESERIALIZE(m_DisplayText, serializer);
+
+        std::string textAlign;
+        DESERIALIZE(textAlign, serializer);
+
+        if (textAlign.compare("left") == 0)
+        {
+            m_TextAlign = TextAlign::Left;
+        }
+        else if (textAlign.compare("center") == 0)
+        {
+            m_TextAlign = TextAlign::Center;
+        }
+        else if (textAlign.compare("right") == 0)
+        {
+            m_TextAlign = TextAlign::Right;
+        }
     }
 
 private:
+    TextAlign m_TextAlign;
     std::string m_DisplayText;
     Mesh m_Mesh;
     Material m_Material;
